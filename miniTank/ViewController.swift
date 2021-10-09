5d29aea968e3bc42124783d2a7b31e1762070109
Emily Zheng <mzheng9@u.rochester.edu>
2021-10-08 23:12:51 -0400

    tank runs and gold brick falls like dominos. Need to fix constraints now

--- miniTank/ViewController.swift
+++ miniTank/ViewController.swift
@@ -7,18 +7,35 @@
 
 import UIKit
 import RealityKit
-
+var tankAnchor: TinyToyTank._TinyToyTank?
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        super.viewDidLoad()
+        tankAnchor=try! TinyToyTank.load_TinyToyTank()
+        tankAnchor!.turret?.setParent(tankAnchor!.tank,preservingWorldTransform: true)
+        arView.scene.anchors.append(tankAnchor!)
+    }
+    @IBAction func turretLeftPressed(_ sender: Any) {
+        tankAnchor!.notifications.turretLeft.post()
+    }
+    @IBAction func turretRightPressed(_ sender: Any) {
+        tankAnchor!.notifications.turretRight.post()
+    }
+    @IBAction func canonFirePressed(_ sender: Any) {
+        tankAnchor!.notifications.cannonFire.post()
+    }
+    @IBAction func tankLeftPressed(_ sender: Any) {
+        tankAnchor!.notifications.tankLeft.post()
+    }
+    
+    @IBAction func tankForwardPressed(_ sender: Any) {
+        tankAnchor!.notifications.tankForward.post()
+    }
+    @IBAction func tankRightPressed(_ sender: Any) {
+        tankAnchor!.notifications.tankRight.post()
     }
 }
